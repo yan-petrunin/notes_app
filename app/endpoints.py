b1e8f28bc0ab16f3d14e19eb4c04f0623ba82600
yan <yan.petrunin@bk.ru>
2024-09-24 19:39:31 +0300

    new crud methods

--- app/endpoints.py
+++ app/endpoints.py
@@ -1,22 +1,35 @@
 import sqlalchemy.exc
 from fastapi import APIRouter
-from databases.postgres.crud import insert_note
+from databases.postgres.crud import insert_note, show_notes
 from app.models import Response
+
+
 router = APIRouter()
-import traceback
+
 
 @router.get('/')
 async def main():
     return 'Hello user!'
 
 
-@router.post('/note')
-async def new_note(header: str, text: str, person_id: int):
+@router.get('/note')
+async def all_notes(user_id: int):
     try:
-        await insert_note(header=header, text=text, person_id=person_id)
+        return await show_notes(user_id=user_id)
+    except sqlalchemy.exc.IntegrityError:
+        return Response(**{"status": "error",
+                           "message": "Ошибка вывода заметок, возможно такой person_id не существует",
+                           "code": 400,
+                           "data": locals()})
+
+
+@router.post('/note')
+async def new_note(header: str, text: str, user_id: int, tags: list | None = None):
+    try:
+        await insert_note(header=header, text=text, user_id=user_id, tags=tags)
         return f'Note inserted'
     except sqlalchemy.exc.IntegrityError:
         return Response(**{"status": "error",
-                           "message": "Ошибка вставки заметки, возможно такой person_id",
+                           "message": "Ошибка вставки заметки, возможно такой person_id не существует",
                            "code": 400,
                            "data": locals()})
--- databases/postgres/crud.py
+++ databases/postgres/crud.py
@@ -1,10 +1,11 @@
-import os
-from sqlalchemy import Select, Insert
 from databases.postgres.models import *
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
 import asyncio
+from sqlalchemy import select
+from sqlalchemy.dialects.postgresql import insert
 import datetime
 from dotenv import load_dotenv
+from typing import Optional
 
 load_dotenv()
 
@@ -12,14 +13,28 @@ engine = create_async_engine(os.getenv('DATABASE_URL'), echo=True)
 AsyncSession = async_sessionmaker(bind=engine, expire_on_commit=False)
 
 
-async def insert_note(header: str, text: str, user_id: int):
+async def insert_note(header: str, text: str, user_id: int, tags: Optional[list] = None):
     async with AsyncSession() as session:
         note = Note(header=header, text=text,
                     creation_date=datetime.datetime.now(),
                     modify_date=datetime.datetime.now(),
                     user_id=user_id)
+        if tags:
+            params = [{"name": tag} for tag in tags]
+            await session.execute(insert(Tag).values(params).on_conflict_do_nothing())
         session.add(note)
         await session.commit()
+
+
+async def update_note(header: Optional[str] = None, text: Optional[str] = None, tags: Optional[list] = None):
+    pass
+
+
+async def show_notes(user_id: int):
+    async with AsyncSession() as session:
+        query = select(Note).where(Note.user_id == user_id)
+        notes = await session.execute(query)
+        return [note.__dict__ for note in notes.scalars().all()]
 
 
 async def insert_user(username: str, email: str, password: str):
@@ -30,4 +45,5 @@ async def insert_user(username: str, email: str, password: str):
 
 if __name__ == '__main__':
     #asyncio.run(insert_user('yan', 'yan@email.com', '12qwer'))
-    asyncio.run(insert_note(header='zxcv', text='vcxz', user_id=2))
+    #asyncio.run(insert_note(header='zxcv', text='vcxz', user_id=2))
+    asyncio.run(show_notes(1))
